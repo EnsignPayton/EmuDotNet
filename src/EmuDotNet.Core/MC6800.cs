2d1e351f41005694882e52d93d024743b9b0e2b3
Logan Payton <Logan.Payton@javs.com>
2021-01-24 01:10:31 -0500

    Registers
    idk may rename later ;)

--- src/EmuDotNet.Core/MC6800.cs
+++ src/EmuDotNet.Core/MC6800.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmuDotNet.Core
+﻿namespace EmuDotNet.Core
 {
     public class MC6800
     {
+        private readonly MC6800Registers _registers = new();
     }
 }
---
+++ src/EmuDotNet.Core/MC6800Registers.cs
@@ -0,0 +1,60 @@
+﻿namespace EmuDotNet.Core
+{
+    public class MC6800Registers
+    {
+        /// <summary>
+        /// Accumulator A
+        /// </summary>
+        public byte A { get; set; }
+
+        /// <summary>
+        /// Accumulator B
+        /// </summary>
+        public byte B { get; set; }
+
+        /// <summary>
+        /// Index Register
+        /// </summary>
+        public ushort IX { get; set; }
+
+        /// <summary>
+        /// Stack Pointer
+        /// </summary>
+        public ushort SP { get; set; }
+
+        /// <summary>
+        /// Program Counter
+        /// </summary>
+        public ushort PC { get; set; }
+
+        /// <summary>
+        /// Half-Carry Flag
+        /// </summary>
+        public bool H { get; set; }
+
+        /// <summary>
+        /// Interrupt Flag
+        /// </summary>
+        public bool I { get; set; }
+
+        /// <summary>
+        /// Sign Flag
+        /// </summary>
+        public bool N { get; set; }
+
+        /// <summary>
+        /// Zero Flag
+        /// </summary>
+        public bool Z { get; set; }
+
+        /// <summary>
+        /// Overflow Flag
+        /// </summary>
+        public bool V { get; set; }
+
+        /// <summary>
+        /// Carry Flag
+        /// </summary>
+        public bool C { get; set; }
+    }
+}
