09f271017eab0a38faa42a826180c63715e2dfe7
Logan Payton <Logan.Payton@javs.com>
2021-01-31 02:04:15 -0500

    and etc

--- src/EmuDotNet.Core/MC6800/Processor.cs
+++ src/EmuDotNet.Core/MC6800/Processor.cs
@@ -46,8 +46,8 @@ namespace EmuDotNet.Core.MC6800
 
         private byte NextExtended()
         {
-            var low = NextImmediate();
             var high = NextImmediate();
+            var low = NextImmediate();
             var address = (high << 8) | low;
             return _memory.GetByte((ushort) address);
         }
@@ -70,22 +70,12 @@ namespace EmuDotNet.Core.MC6800
             _ => throw new ArgumentException($"Addressing mode {mode} does not read from memory", nameof(mode))
         };
 
-        private byte Carry()
-        {
-            return _registers.C ? 1 : 0;
-        }
-
         private void OperateOn(Accumulator reg, Func<byte, byte> func)
         {
             if (reg == Accumulator.A)
                 _registers.A = func(_registers.A);
             else
                 _registers.B = func(_registers.B);
-        }
-
-        private void OperateOn(Accumulator reg, Func<byte, int> func)
-        {
-            OperateOn(reg, x => (byte) func(x));
         }
 
         private void Execute(Instruction instruction)
@@ -115,25 +105,19 @@ namespace EmuDotNet.Core.MC6800
                 case Instruction.ADC_B_EXT:
                     AddWithCarry(instruction.GetAccumulator(), instruction.GetMode());
                     break;
+                case Instruction.AND_A_IMM:
+                case Instruction.AND_A_DIR:
+                case Instruction.AND_A_IDX:
+                case Instruction.AND_A_EXT:
+                case Instruction.AND_B_IMM:
+                case Instruction.AND_B_DIR:
+                case Instruction.AND_B_IDX:
+                case Instruction.AND_B_EXT:
+                    And(instruction.GetAccumulator(), instruction.GetMode());
+                    break;
                 default:
                     return;
             }
-        }
-
-        private byte AdcAdd(byte val1, byte val2)
-        {
-            var low1 = val1 & 0xF;
-            var low2 = val2 & 0xF;
-            var lowSum = low1 + low2;
-            _registers.H = (lowSum & 0x10) != 0;
-
-            var sum = val1 + val2;
-            _registers.N = (sum & 0x80) != 0;
-            _registers.Z = sum == 0;
-            _registers.V = (val1 & 0x80) == 0 && (val2 & 0x80) == 0 && (sum & 0x80) != 0 ||
-                           (val1 & 0x80) != 0 && (val2 & 0x80) != 0 && (sum & 0x80) == 0;
-            _registers.C = (sum & 0x100) != 0;
-            return (byte) sum;
         }
 
         private void Add(Accumulator reg, AddressingMode mode)
@@ -143,12 +127,43 @@ namespace EmuDotNet.Core.MC6800
 
         private void AddAccumulators()
         {
-            _registers.A = AdcAdd(_registers.A, _registers.B);
+            OperateOn(Accumulator.A, x => AdcAdd(x, _registers.B));
         }
 
         private void AddWithCarry(Accumulator reg, AddressingMode mode)
         {
-            OperateOn(reg, x => x + NextValue(mode) + Carry());
+            OperateOn(reg, x => AdcAdd(x, NextValue(mode), true));
+        }
+
+        private byte AdcAdd(byte val1, byte val2, bool useCarry = false)
+        {
+            var carry = useCarry && _registers.C ? 1 : 0;
+
+            var low1 = val1 & 0xF;
+            var low2 = val2 & 0xF;
+            var lowSum = low1 + low2 + carry;
+            _registers.H = (lowSum & 0x10) != 0;
+
+            var sum = val1 + val2 + carry;
+            _registers.N = (sum & 0x80) != 0;
+            _registers.Z = sum == 0;
+            _registers.V = (val1 & 0x80) == 0 && (val2 & 0x80) == 0 && (sum & 0x80) != 0 ||
+                           (val1 & 0x80) != 0 && (val2 & 0x80) != 0 && (sum & 0x80) == 0;
+            _registers.C = (sum & 0x100) != 0;
+            return (byte)sum;
+        }
+
+        private void And(Accumulator reg, AddressingMode mode)
+        {
+            OperateOn(reg, x => AdcAnd(x, NextValue(mode)));
+        }
+
+        private byte AdcAnd(byte val1, byte val2)
+        {
+            var and = val1 & val2;
+            _registers.N = (and & 0x80) != 0;
+            _registers.Z = and == 0;
+            return (byte) and;
         }
     }
 }
--- test/EmuDotNet.Core.Test/MC6800/ProcessorTests.cs
+++ test/EmuDotNet.Core.Test/MC6800/ProcessorTests.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using EmuDotNet.Core.MC6800;
+﻿using EmuDotNet.Core.MC6800;
 using Xunit;
 
 namespace EmuDotNet.Core.Test.MC6800
 {
     public class ProcessorTests
     {
-        #region Add Instructions
-
         [Fact]
         public void Add_Sets_Half_Carry()
         {
@@ -75,7 +72,116 @@ namespace EmuDotNet.Core.Test.MC6800
             Assert.True(target.Registers.C);
         }
 
-        #endregion
+        [Fact]
+        public void Add_With_Carry_Includes_Carry()
+        {
+            var target = GetTestTarget(
+                (byte) Instruction.ADD_A_IMM, 0x80,
+                (byte) Instruction.ADD_A_IMM, 0x80,
+                (byte) Instruction.ADC_A_IMM, 0x01);
+
+            target.ExecuteClock();
+            target.ExecuteClock();
+            target.ExecuteClock();
+
+            Assert.Equal(0x02, target.Registers.A);
+        }
+
+        [Fact]
+        public void Add_Accumulators_Works()
+        {
+            var target = GetTestTarget((byte) Instruction.ABA);
+
+            target.Registers.A = 0x0F;
+            target.Registers.B = 0x01;
+            target.ExecuteClock();
+
+            Assert.Equal(0x10, target.Registers.A);
+        }
+
+        [Fact]
+        public void Add_Direct_Loads_Data()
+        {
+            var target = GetTestTarget(
+                (byte) Instruction.ADD_A_DIR, 0x04, 0x00, 0x00, 0x69);
+
+            target.ExecuteClock();
+
+            Assert.Equal(0x69, target.Registers.A);
+        }
+
+        [Fact]
+        public void Add_Extended_Loads_Data()
+        {
+            var target = GetTestTarget(
+                (byte)Instruction.ADD_A_EXT, 0x00, 0x04, 0x00, 0x69);
+
+            target.ExecuteClock();
+
+            Assert.Equal(0x69, target.Registers.A);
+        }
+
+        [Fact]
+        public void Add_Indexed_Loads_Data()
+        {
+            var target = GetTestTarget(
+                (byte) Instruction.ADD_A_IDX, 0x01, 0x00, 0x00, 0x69);
+
+            target.Registers.IX = 0x03;
+            target.ExecuteClock();
+
+            Assert.Equal(0x69, target.Registers.A);
+        }
+
+        [Fact]
+        public void And_Works()
+        {
+            var target = GetTestTarget(
+                (byte) Instruction.AND_A_IMM, 0xAA);
+
+            target.Registers.A = 0x0F;
+            target.ExecuteClock();
+
+            Assert.Equal(0x0A, target.Registers.A);
+        }
+
+        [Fact]
+        public void And_Sets_Sign_Flag()
+        {
+            var target = GetTestTarget(
+                (byte)Instruction.AND_A_IMM, 0xAA);
+
+            target.Registers.A = 0xF0;
+            target.ExecuteClock();
+
+            Assert.Equal(0xA0, target.Registers.A);
+            Assert.True(target.Registers.N);
+        }
+
+        [Fact]
+        public void And_Sets_Zero_Flag()
+        {
+            var target = GetTestTarget(
+                (byte)Instruction.AND_A_IMM, 0xAA);
+
+            target.ExecuteClock();
+
+            Assert.Equal(0x00, target.Registers.A);
+            Assert.True(target.Registers.Z);
+        }
+
+        [Fact]
+        public void And_Clears_Overflow_Flag()
+        {
+            var target = GetTestTarget(
+                (byte)Instruction.AND_A_IMM, 0xFF);
+
+            target.Registers.A = 0xFF;
+            target.ExecuteClock();
+
+            Assert.Equal(0xFF, target.Registers.A);
+            Assert.False(target.Registers.V);
+        }
 
         private static Processor GetTestTarget(params byte[] data) =>
             new Processor(new SimpleMemory(data));
