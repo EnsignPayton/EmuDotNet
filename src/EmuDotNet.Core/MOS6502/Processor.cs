ae35e4d2610f6013f276c256d4e5ed9cd08a7cda
Logan Payton <Logan.Payton@javs.com>
2022-05-01 23:57:58 -0400

    Stinky but functional

--- src/EmuDotNet.Core/MOS6502/Processor.cs
+++ src/EmuDotNet.Core/MOS6502/Processor.cs
@@ -5,6 +5,7 @@ namespace EmuDotNet.Core.MOS6502;
 public class Processor : IProcessor
 {
     private readonly Registers _reg = new();
+    private readonly ALU _alu;
     private readonly IBus _bus;
     private int _cycles;
 
@@ -14,6 +15,7 @@ public class Processor : IProcessor
 
     public Processor(IBus bus)
     {
+        _alu = new ALU(_reg);
         _bus = bus;
         Reset();
     }
@@ -22,27 +24,13 @@ public class Processor : IProcessor
     {
         if (!IsExecuting)
         {
-            var opcodeByte = ReadImmediate();
-            var opcode = OpcodeParser.Parse(opcodeByte);
+            var val = _bus[_reg.PC];
+            _reg.PC++;
+            _cycles++;
 
-            byte operand;
-            if (opcode.AddressMode == AddressMode.IMM)
-            {
-                operand = ReadImmediate();
-            }
-            else if (opcode.AddressMode == AddressMode.ZPG)
-            {
-                operand = ReadZeroPage();
-            }
-            else
-            {
-                operand = ReadImplied();
-            }
-
-            if (opcode.Instruction == Instruction.ADC)
-            {
-                AddWithCarry(operand);
-            }
+            var (instruction, mode) = OpcodeParser.Parse(val);
+            var operand = ReadOperand(mode);
+            Execute(instruction, operand);
         }
 
         _cycles--;
@@ -56,105 +44,144 @@ public class Processor : IProcessor
 
     public void Interrupt()
     {
-        // TODO: This
+        throw new NotImplementedException();
     }
 
     public void InterruptNonMaskable()
     {
-        // TODO: This
+        throw new NotImplementedException();
     }
-    
-    #region Bus Reads
 
-    // 2 cycles
-    private ushort ReadAbsolute()
+    private byte ReadOperand(AddressMode mode)
     {
-        var low = ReadImmediate();
-        var high = ReadImmediate();
-        return (ushort)((high << 8) | low);
+        switch (mode)
+        {
+            case AddressMode.IMP:
+                return _reg.A;
+            case AddressMode.IMM:
+            {
+                var val = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                return val;
+            }
+            case AddressMode.ZPG:
+            {
+                var address = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.ZPX:
+            {
+                var addressBase = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var offset = _reg.X;
+                var address = (byte)((addressBase + offset) & 0xFF);
+                _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.ABS:
+            {
+                var low = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var high = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var address = (ushort) ((high << 8) + low);
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.ABX:
+            {
+                var low = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var high = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var addressBase = (ushort) ((high << 8) + low);
+                var address = (ushort) (addressBase + _reg.X);
+                if (low + _reg.X > 256) _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.ABY:
+            {
+                var low = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var high = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var addressBase = (ushort) ((high << 8) + low);
+                var address = (ushort) (addressBase + _reg.Y);
+                if (low + _reg.Y > 256) _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.INX:
+            {
+                var zAddressBase = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var zAddress = (ushort) ((zAddressBase + _reg.X) & 0xFF);
+                var low = _bus[zAddress];
+                _cycles++;
+                var high = _bus[(ushort) (zAddress + 1)];
+                _cycles++;
+                var address = (ushort) ((high << 8) + low);
+                _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            case AddressMode.INY:
+            {
+                var zAddress = _bus[_reg.PC];
+                _reg.PC++;
+                _cycles++;
+                var addressBaseLow = _bus[zAddress];
+                _cycles++;
+                var addressBaseHigh = _bus[(ushort) (zAddress + 1)];
+                _cycles++;
+                var addressBase = (ushort) ((addressBaseHigh << 8) + addressBaseLow);
+                var address = (ushort) (addressBase + _reg.Y);
+                if ((addressBaseLow + _reg.Y) > 256) _cycles++;
+                var val = _bus[address];
+                _cycles++;
+                return val;
+            }
+            default:
+                throw new NotImplementedException();
+        }
     }
 
-    // 4-5 cycles
-    private ushort ReadAbsoluteXOffset(out int extraCycles) =>
-        ReadAbsoluteOffset(_reg.X, out extraCycles);
-
-    // 4-5 cycles
-    private ushort ReadAbsoluteYOffset(out int extraCycles) =>
-        ReadAbsoluteOffset(_reg.Y, out extraCycles);
-    
-    private ushort ReadAbsoluteOffset(byte reg, out int extraCycles)
+    private void Execute(Instruction instruction, byte operand)
     {
-        var temp = ReadAbsolute();
-        var result = (ushort)(temp + reg);
-        extraCycles = (temp & 0xFF) + reg > 0xFF ? 1 : 0;
-        return result;
+        if (false)
+        {
+        }
+        else if (instruction == Instruction.ADC)
+        {
+            _alu.AddWithCarry(operand);
+        }
+        else if (instruction == Instruction.LDA)
+        {
+            _reg.A = operand;
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    private byte ReadImmediate()
-    {
-        var value = _bus[_reg.PC];
-        _reg.PC += 1;
-        _cycles++;
-        return value;
-    }
-
-    private byte ReadImplied()
-    {
-        // Some opcodes use accumulator as implied value. It's the only one, so just return it
-        return _reg.A;
-    }
-
-    private ushort ReadIndirect()
-    {
-        var temp = ReadAbsolute();
-
-        // Do not cross page boundary - mod 256
-        var highAddress = (temp & 0xFF) != 0xFF
-            ? (ushort) (temp + 1)
-            : (ushort) (temp & 0xFF00);
-
-        var low = _bus[temp];
-        var high = _bus[highAddress];
-
-        return (ushort)((high << 8) | low);
-    }
-    
-    // INX, INY
-
-    private ushort ReadRelative()
-    {
-        // Return signed offset from PC, but still store it in a ushort for consistency
-        ushort temp = ReadImmediate();
-        return (temp & 0x80) != 0
-            ? (ushort) (temp | 0xFF00)
-            : temp;
-    }
-
-    private byte ReadZeroPage()
-    {
-        var address = ReadImmediate();
-        var value = _bus[address];
-        _cycles++;
-        return value;
-    }
-
-    #endregion
-    
-    #region Opcode Execution
-
-    private void AddWithCarry(byte value)
-    {
-        // A + M + C -> A, C
-        var result = _reg.A + value + (_reg.C ? 1 : 0);
-        var operandsSignMatch = (_reg.A & 0x80) == (value & 0x80);
-        var resultSignMatch = (_reg.A & 0x80) == (result & 0x80);
-        
-        _reg.A = (byte) (result & 0xFF);
-        _reg.C = (result & 0xFF00) != 0;
-        _reg.Z = result == 0;
-        _reg.V = operandsSignMatch && !resultSignMatch;
-        _reg.N = (result & 0x80) > 0;
-    }
-
-    #endregion
 }
--- test/EmuDotNet.Core.Test/MOS6502/ProcessorTests.cs
+++ test/EmuDotNet.Core.Test/MOS6502/ProcessorTests.cs
@@ -2,5 +2,210 @@
 
 public class ProcessorTests
 {
-    private static Processor GetTarget() => new(new SimpleBus());
+    [Fact]
+    public void ADC_IMM()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Bus[0x8000] = 0x69;
+        target.Bus[0x8001] = 0x11;
+
+        ExecuteCycles(target, 2);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ZPG()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Bus[0x0015] = 0x11;
+        target.Bus[0x8000] = 0x65;
+        target.Bus[0x8001] = 0x15;
+
+        ExecuteCycles(target, 3);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ZPX()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.X = 0x04;
+        target.Bus[0x0019] = 0x11;
+        target.Bus[0x8000] = 0x75;
+        target.Bus[0x8001] = 0x15;
+
+        ExecuteCycles(target, 4);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ABS()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Bus[0x1234] = 0x11;
+        target.Bus[0x8000] = 0x6D;
+        target.Bus[0x8001] = 0x34;
+        target.Bus[0x8002] = 0x12;
+
+        ExecuteCycles(target, 4);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ABX()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.X = 0x04;
+        target.Bus[0x1238] = 0x11;
+        target.Bus[0x8000] = 0x7D;
+        target.Bus[0x8001] = 0x34;
+        target.Bus[0x8002] = 0x12;
+
+        ExecuteCycles(target, 4);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ABX_PageCross()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.X = 0x04;
+        target.Bus[0x1203] = 0x11;
+        target.Bus[0x8000] = 0x7D;
+        target.Bus[0x8001] = 0xFF;
+        target.Bus[0x8002] = 0x11;
+
+        ExecuteCycles(target, 5);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ABY()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.Y = 0x04;
+        target.Bus[0x1238] = 0x11;
+        target.Bus[0x8000] = 0x79;
+        target.Bus[0x8001] = 0x34;
+        target.Bus[0x8002] = 0x12;
+
+        ExecuteCycles(target, 4);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_ABY_PageCross()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.Y = 0x04;
+        target.Bus[0x1203] = 0x11;
+        target.Bus[0x8000] = 0x79;
+        target.Bus[0x8001] = 0xFF;
+        target.Bus[0x8002] = 0x11;
+
+        ExecuteCycles(target, 5);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_INX()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.X = 0x04;
+        target.Bus[0x0014] = 0x34;
+        target.Bus[0x0015] = 0x12;
+        target.Bus[0x1234] = 0x11;
+        target.Bus[0x8000] = 0x61;
+        target.Bus[0x8001] = 0x10;
+
+        ExecuteCycles(target, 6);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_INY()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.Y = 0x04;
+        target.Bus[0x0010] = 0x34;
+        target.Bus[0x0011] = 0x12;
+        target.Bus[0x1238] = 0x11;
+        target.Bus[0x8000] = 0x71;
+        target.Bus[0x8001] = 0x10;
+
+        ExecuteCycles(target, 5);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    [Fact]
+    public void ADC_INY_PageCross()
+    {
+        var target = GetTarget();
+        target.Registers.A = 0xA5;
+        target.Registers.Y = 0x04;
+        target.Bus[0x0010] = 0xFF;
+        target.Bus[0x0011] = 0x11;
+        target.Bus[0x1203] = 0x11;
+        target.Bus[0x8000] = 0x71;
+        target.Bus[0x8001] = 0x10;
+
+        ExecuteCycles(target, 6);
+
+        Assert.Equal(0xB6, target.Registers.A);
+    }
+
+    // TODO: In Order, etc
+
+    [Fact]
+    public void LDA_Loads_Immediate()
+    {
+        var target = GetTarget();
+        target.Registers.PC = 0x8000;
+        target.Bus[0x8000] = 0xA9;
+        target.Bus[0x8001] = 0xA5;
+
+        ExecuteCycles(target, 2);
+
+        Assert.Equal(0xA5, target.Registers.A);
+    }
+
+    private static void ExecuteCycles(Processor target, int cycles)
+    {
+        target.ExecuteClock();
+
+        for (int i = 1; i < cycles; i++)
+        {
+            Assert.True(target.IsExecuting);
+            target.ExecuteClock();
+        }
+
+        Assert.False(target.IsExecuting);
+    }
+
+    private static Processor GetTarget()
+    {
+        var target = new Processor(new SimpleBus());
+        target.Registers.PC = 0x8000;
+        return target;
+    }
 }
